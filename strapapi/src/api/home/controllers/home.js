93b7012688947f6a21b3bcc2c78f7584f12cdd48
Sebastian-ctr <sebastiansmal@gmail.com>
2022-01-08 15:39:55 +0100

    back to default controllers

--- strapapi/src/api/home/controllers/home.js
+++ strapapi/src/api/home/controllers/home.js
@@ -8,25 +8,7 @@ const request = require('request')
 const { createCoreController } = require('@strapi/strapi').factories;
 
 
-module.exports = createCoreController('api::home.home', ({strapi }) => ({
-    async find(ctx) {
-        const qs = require('qs');
-        const query = qs.stringify({
-        populate: '*', 
-        }, {
-        encodeValuesOnly: true,
-        });
-
-        await request(`/api/homes?${query}`);
-        // GET /api/articles?populate=%2A
-
-        const { data, meta } = await super.find(ctx)
-
-        meta.data = Data.now()
-
-        return {data, meta}
-    }
-}));
+module.exports = createCoreController('api::home.home');
 
 
 
